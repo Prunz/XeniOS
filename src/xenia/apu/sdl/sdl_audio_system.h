bffb6c0a1184122e19c600d26aa6b5b91cb3acef
Prunz <84547572+Prunz@users.noreply.github.com>
2026-03-25 23:09:45 -0500

    Change actual_channels to a fixed value of 6

--- src/xenia/apu/sdl/sdl_audio_system.h
+++ src/xenia/apu/sdl/sdl_audio_system.h
@@ -65,7 +65,7 @@ class SDLAudioSystem : public AudioSystem {
     // Actual channel count for this slot. Guest XMA clients always use
     // kFrameChannelsDefault (6). The media player uses the channel count
     // reported by FFmpeg (commonly 2 for stereo music).
-    uint32_t actual_channels = AudioDriver::kFrameChannelsDefault;
+    uint32_t actual_channels = 6;  // kFrameChannelsDefault
   };
 
   void MixSlotSubmit(size_t slot_index, float* frame);
